8f50ba4fc06cef0826d508289506200cd4b6c120
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-01-18 18:43:14 -0500

    set up route that returns the dataBase array in index.js

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -3,3 +3,8 @@ let dataBase = require ("../db/db.json")
 const fs = require('fs')
 
 const { v4: uuidv4 } = require('uuid');
+//setting up a route for a GET request to '/notes' endpoint.
+router.get('/notes', (req, res) =>{
+    console.log('route hit')
+    res.json(dataBase);
+});
