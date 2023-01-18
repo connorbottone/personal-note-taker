e5d0fcfa3459e66a03eee8edbe777c4de9c75205
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-01-18 18:02:21 -0500

    set up middleware

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+//requireing all necassary e xt
 let router = require ("express").Router()
 let dataBase = require ("../db/db.json")
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
